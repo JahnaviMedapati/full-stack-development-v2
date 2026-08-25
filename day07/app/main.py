40f80706b28ffae671ee8245e3097dbf06eff969
jahnavi <keerthijahnavi60@email.com>
2026-08-25 09:23:21 +0530

    modified day07

--- day07/app/main.py
+++ day07/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.tasks import router as task_router
 
 
 app = FastAPI(
     title="Task Manager API"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
